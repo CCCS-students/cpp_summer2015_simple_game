180073b817e14b137421e97fc08266536e7d22c4
feglenn1 <feglenn1@cougars.ccis.edu>
2015-05-26 16:31:24 -0500

    Add goblin.cpp

--- src/goblin/goblin.cpp
+++ src/goblin/goblin.cpp
@@ -1,42 +1,10 @@
+#include <iostream>
 #include "goblin.h"
-#include <string>
-Goblin::Goblin(double sizeIn, string colorIn, string weaponIn, string statusIn)
-{
-	size = sizeIn;
-	color = colorIn;
-	weapon = weaponIn;
-	status = statusIn;
-}
 
-void Goblin::set_size(double sizeIn)
-{
-	size = sizeIn;
-}
-std::string Goblin::get_size()
-{
-	return sizeIn;
-}
-void Goblin::set_color(string colorIn)
-{
-	color = colorIn;
-}
-int Goblin::get_color()
-{
-	return color;
-}
-void Goblin::set_weapon(string weaponIn)
-{
-	weapon = weaponIn;
-}
-int Goblin::get_weapon()
-{
-	return weapon;
-}
-void Goblin::set_status(string statusIn)
-{
-	status = statusIn;
-}
-int Goblin::get_status()
-{
-	return status;
-}
+Goblin::Goblin(int health_, int current_room_,
+    bool friend_)
+    : health(health_), current_room(current_room_), friend(friend_)
+{}
+
+
+
