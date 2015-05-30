07eaabbe8026ae9af4ce8210d65933cb81fd0b25
LolingLenin <sergeygruzdev98@gmail.com>
2015-05-30 15:38:27 -0500

    Update Room.h to include height, width, x-coordinate, y-coordinate and some includes.

--- src/room/Room.h
+++ src/room/Room.h
@@ -1,11 +1,26 @@
 #ifndef ROOM_H
 #define ROOM_H
 
+#include <cstdlib.h>
+#include <vector.h>
+
+#include <Tree.h>
+#include <Goblin.h>
+#include <Chest.h>
+
 class Room
 {
 public:
     Room();
     ~Room();
+
+private:
+    int rHeight;
+    int rWidth;
+    int rPosX;
+    int rPosY;
+    // Some sort of container to store objects (tree, goblin, chest, etc.)
+    // Texture filepath
 };
 
 #endif
