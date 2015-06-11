42d058a8c13f9497e9f26b1b8df55d5a0a88f230
LolingLenin <sergeygruzdev98@gmail.com>
2015-06-10 20:45:39 -0500

    Update the TestCow file to do more of a visual test. Will probabl scrap later

--- src/cow/TestCow.cpp
+++ src/cow/TestCow.cpp
@@ -1,8 +1,37 @@
 #include "Cow.h"
-#include <iostream>	
+#include <iostream>
+#include <cstdlib>
 
 int main()
 {
+	srand(time(0));
 	Cow aCow(0, 0, "Cow/pics/cow.bmp", "Hamburger");
-	aCow.moo(10);b
+	// Main loop
+	for(int i = 0; i < 100; i ++)
+	{
+		// Disp cow
+		for(int i = 0; i < aCow.getY(); i ++)
+		{
+			std::cout << "\n";
+		}
+	
+		for(int i = 0; i < aCow.getX(); i ++)
+		{
+			std::cout << " ";
+		}
+		
+		std::cout << aCow.getName();
+
+		// Move cow
+		aCow.move(1, 1);
+	
+		// Moo
+		std::cout << std::endl << std::endl;
+		aCow.moo(rand() % 10 + 2);
+		
+		std::cin.get();
+
+		for(int i = 0; i < 5; i ++)
+			std::cout << "\n";
+	}
 }
