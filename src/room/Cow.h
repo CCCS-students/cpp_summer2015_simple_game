6c9751f4869743831e6e327b9b6ada6a8fccf591
LolingLenin <sergeygruzdev98@gmail.com>
2015-06-13 14:41:45 -0500

    Alter Cow.h in room folder to undo changes, which were supposed to be in the cow folder. Now the testRoom.cpp compiles without errors

--- src/room/Cow.h
+++ src/room/Cow.h
@@ -7,17 +7,7 @@ class Cow
 	public:
 	Cow();
 	~Cow();
-	
-	
 
-	private:
-	// Filepath for the cow texture
-	std::string picFilePath;
-	// A name for the cow
 	std::string name;
-	// Coordinates of the cow's position
-	int x;
-	int y;
-
 };
 #endif
