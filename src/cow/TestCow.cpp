89dce423937b94bebce37aa6be791b982b7e18f7
LolingLenin <sergeygruzdev98@gmail.com>
2015-06-13 14:48:17 -0500

    Scrap idea to create text-based visual tests for cow class, will resort instead to simple textual information output

--- src/cow/TestCow.cpp
+++ src/cow/TestCow.cpp
@@ -1,37 +1,11 @@
 #include "Cow.h"
 #include <iostream>
-#include <cstdlib>
+
 
 int main()
 {
-	srand(time(0));
 	Cow aCow(0, 0, "Cow/pics/cow.bmp", "Hamburger");
-	// Main loop
-	for(int i = 0; i < 100; i ++)
-	{
-		// Disp cow
-		for(int i = 0; i < aCow.getY(); i ++)
-		{
-			std::cout << "\n";
-		}
-	
-		for(int i = 0; i < aCow.getX(); i ++)
-		{
-			std::cout << " ";
-		}
-		
-		std::cout << aCow.getName();
-
-		// Move cow
-		aCow.move(1, 1);
-	
-		// Moo
-		std::cout << std::endl << std::endl;
-		aCow.moo(rand() % 10 + 2);
-		
-		std::cin.get();
-
-		for(int i = 0; i < 5; i ++)
-			std::cout << "\n";
-	}
+	std::cout << "Created a cow, details:\n\n";
+	std::cout << "Name: " << aCow.getName() << "\n";
+	std::cout << "Texture location: " << 
 }
